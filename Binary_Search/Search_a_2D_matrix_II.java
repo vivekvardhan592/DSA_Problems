8aff70cabb91bdf2408660dd82b4a7ecf1b3581d
vivekvardhan592 <vivekvardhan592@gmail.com>
2026-03-12 22:24:53 +0530

    Search a 2D Matrix and Find Peak Element added

---
+++ Binary_Search/Search_a_2D_matrix_II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int row=0;
+        int col=n-1;
+        while(row<m && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+
+    }
+}
+
+
+//Time complexity - O(m+n)
+//Space complexity-O(1)
