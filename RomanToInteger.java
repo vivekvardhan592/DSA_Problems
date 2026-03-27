004984540f4d813d508e1a0c7e709c83c1689faf
vivekvardhan592 <vivekvardhan592@gmail.com>
2026-03-27 23:45:09 +0530

    Roman to Int problem is solved

---
+++ RomanToInteger.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                total -= current;
+            } else {
+                total += current;
+            }
+        }
+
+        return total;
+    }
+
+    private int value(char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+        return 0;
+    }
+}
