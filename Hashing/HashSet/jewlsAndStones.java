6abd117d1918098f19d91a2b37dec127c7cccba9
vivekvardhan592 <vivekvardhan592@gmail.com>
2026-04-05 21:11:34 +0530

    jewels and stones problem solved

---
+++ Hashing/HashSet/jewlsAndStones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+      int count=0;
+      HashSet<Character> set=new HashSet<>();
+      for(char ch:jewels.toCharArray()){
+        set.add(ch);
+      }  
+      for(char ch:stones.toCharArray()){
+        if(set.contains(ch)){
+            count++;
+        }
+      }
+      return count;
+    }
+}
+
+// Time-O(m+n)
+// Space-O(n)
